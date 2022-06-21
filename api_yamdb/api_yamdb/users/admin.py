26e77d9ad4ef2a0c10e8aa95a3c14f0a69a20153
Vsevolod Vishnyakov <vsevolod.vishnyakov@gmail.com>
2022-06-21 21:20:09 +0300

    users #1

---
+++ api_yamdb/api_yamdb/users/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from .models import User
+
+
+class BaseAdminSettings(admin.ModelAdmin):
+    """Панель Админа"""
+    empty_value_display = '-empty-'
+    list_filter = ('role', 'username')
+
+
+class UsersAdmin(BaseAdminSettings):
+    """Оформление панели админа"""
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'role',
+        'bio',
+    )
+    list_display_links = ('id', 'username')
+    search_fields = ('role', 'username')
+
+
+admin.site.register(User, UsersAdmin)
